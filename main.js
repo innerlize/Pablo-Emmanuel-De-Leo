5e00abc47b3561e38c88fce48d692f31f8f100a5
Alan Aguilar <93650566+tiltedveggie@users.noreply.github.com>
2021-12-02 16:38:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	$(".nav-button").click(function() {
+		$(".stick1").toggleClass("selected")
+		$(".stick2").toggleClass("selected2")
+		$(".nav-list-item").toggleClass("selected4")
+	})
+
+	// const year = new Date()
+
+	// document.getElementById("year").innerHTML = year.getFullYear()
+
+})
