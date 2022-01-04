21f808720c898389691be1ff47074680a9a1ac43
Alan Aguilar <93650566+tiltedveggie@users.noreply.github.com>
2022-01-04 17:51:21 -0300

    Delete button.js

--- button.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-	$(".nav-button").click(function() {
-		$(".stick1").toggleClass("selected")
-		$(".stick2").toggleClass("selected2")
-		$(".nav-list-item").toggleClass("selected4")
-	})
-
-})
