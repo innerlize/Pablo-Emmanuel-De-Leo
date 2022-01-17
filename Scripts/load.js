e896761688b7bea2b7df8a39ae692f2ebb8af656
Alan Aguilar <93650566+tiltedveggie@users.noreply.github.com>
2022-01-17 18:48:35 -0300

    Delete load.js

--- Scripts/load.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-     $('#para1').addClass('onLoad2')
-     $('#para2').addClass('onLoad3')
-     $('.header-social').addClass('onLoad3')
-})
-
-$(window).on('load', function() {
-     $(".loading-screen").fadeOut("slow");
-})
