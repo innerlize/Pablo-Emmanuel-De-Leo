6e9e5d36b7b5debed5627233ad2fd4408808ca89
Alan Aguilar <93650566+tiltedveggie@users.noreply.github.com>
2022-01-28 22:32:25 -0300

    Update pushAttr.js

--- Scripts/pushAttr.js
+++ Scripts/pushAttr.js
@@ -1,1 +1,1 @@
-$('.project-containerMob .project img').attr('data-aos-ancho-placement', 'bottom-bottom');
+$('.project-containerMob .project img').attr('data-aos-anchor-placement', 'bottom-bottom');
